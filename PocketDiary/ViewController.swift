4e6328c1f8586c36be3921c9e3672ab2164058ed
Sweeper777 <sumulang@gmail.com>
2016-06-03 20:43:04 +0800

    data model and some basic stuff

--- PocketDiary/ViewController.swift
+++ PocketDiary/ViewController.swift
@@ -1,25 +1,6 @@
-//
-//  ViewController.swift
-//  PocketDiary
-//
-//  Created by Mulang Su on 6/3/16.
-//  Copyright © 2016 Mulang Su. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
