526753bce64546bbb164435d0cd462ef46dcca14
Sweeper777 <sumulang@gmail.com>
2016-06-03 21:02:39 +0800

    added FSCalendar

---
+++ Pods/Target Support Files/Pods-PocketDiary/Pods-PocketDiary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PocketDiary : NSObject
+@end
+@implementation PodsDummy_Pods_PocketDiary
+@end
---
+++ Pods/Target Support Files/Pods-PocketDiary/Pods-PocketDiary-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PocketDiaryVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PocketDiaryVersionString[];
+
