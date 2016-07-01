e8579640d523f28159b13a146d5716f5a6b9bba4
Sweeper777 <sumulang@gmail.com>
2016-07-01 10:54:28 +0800

    updated htmlDescription to support bgColor

--- PocketDiary/Entry.swift
+++ PocketDiary/Entry.swift
@@ -40,7 +40,39 @@ class Entry: NSManagedObject {
         let displayTitleAndContent = mode == .TitleAndContent ? "<span id=\"searchtext\"><h1>\(displayTitle)</h1>\(displayContent)</span>" : "<h1>\(displayTitle)</h1>\(displayContent)"
         let displayHtml = "\(dateFormatted)<hr>\(displayTitleAndContent)"
         
-        let ret = "<style>\(stylesheet)</style> \(displayHtml.emojiUnescapedString)"
+        let displayHtmlWithColor = bgColor == nil ? displayHtml : "<body style=\"background: rgb(\(self.bgColor!.toColor().redComponent), \(self.bgColor!.toColor().greenComponent), \(self.bgColor!.toColor().blueComponent))\">\(displayHtml)</body>"
+        
+        let ret = "<style>\(stylesheet)</style> \(displayHtmlWithColor.emojiUnescapedString)"
         return ret
+    }
+    
+    var htmlDescription: String {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .LongStyle
+        formatter.timeStyle = .NoStyle
+        let dateFormatted = formatter.stringFromDate(date!)
+        
+        let stylesheet = try! String(contentsOfFile: NSBundle.mainBundle().pathForResource("modest", ofType: "css")!)
+        
+        let contentHtml = (try? MMMarkdown.HTMLStringWithMarkdown(content!, extensions: .GitHubFlavored)) ?? content!
+        let displayTitleAndContent = "<h1>\(title!)</h1>\(contentHtml)"
+        let displayHtml = "\(dateFormatted)<hr>\(displayTitleAndContent)"
+        
+        let displayHtmlWithColor = bgColor == nil ? displayHtml : "<body style=\"background: rgb(\(self.bgColor!.toColor().redComponent), \(self.bgColor!.toColor().greenComponent), \(self.bgColor!.toColor().blueComponent))\">\(displayHtml)</body>"
+        
+        let ret = "<style>\(stylesheet)</style> \(displayHtmlWithColor.emojiUnescapedString)"
+        return ret
+    }
+}
+
+extension NSNumber {
+    func toColor() -> UIColor {
+        let rgbValue = self.intValue
+        let red =   CGFloat((rgbValue & 0xFF0000) >> 16) / 0xFF
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 0xFF
+        let blue =  CGFloat(rgbValue & 0x0000FF) / 0xFF
+        let alpha = CGFloat(1.0)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
