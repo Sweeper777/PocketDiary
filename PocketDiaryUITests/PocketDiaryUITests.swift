354cb88d8d7862d7bc89d3ab7683ad84302586aa
Sweeper777 <sumulang@gmail.com>
2016-07-08 13:01:19 +0800

    added some really bad screenshots and UI tests

--- PocketDiaryUITests/PocketDiaryUITests.swift
+++ PocketDiaryUITests/PocketDiaryUITests.swift
@@ -23,24 +23,53 @@ class PocketDiaryUITests: XCTestCase {
         if deviceLanguage.hasPrefix("en") {
             num = "26"
         } else if deviceLanguage.hasSuffix("Hans") {
-        
+            num = "15"
+        } else if deviceLanguage.hasSuffix("Hant") {
+            num = "14"
         }
-        let staticText = app.collectionViews.staticTexts[num]
-        staticText.tap()
+        
+    
+        app.collectionViews.staticTexts[num].tap()
         snapshot("2preview")
-        app.buttons["Editor"].tap()
+        app.buttons.elementBoundByIndex(5).tap()
         snapshot("3editor")
-        app.navigationBars["6/26/16"].buttons["Cancel"].tap()
-        app.navigationBars["My Diaries"].buttons["search filled"].tap()
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(1).tap()
+            app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(1).tap()
+        } else {
+            app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(0).tap()
+            app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(1).tap()
+        }
         
         let tablesQuery = app.tables
-        tablesQuery.textFields["Search"].tap()
-        tablesQuery.textFields["Search"].typeText("beach")
-        snapshot("4search")
-        app.navigationBars["Search"].buttons["search colored"].tap()
-        app.navigationBars["Results - 1 / 2"].buttons["right"].tap()
+        tablesQuery.textFields.elementBoundByIndex(0).tap()
         
-        snapshot("5result")
+        var text = ""
+        
+        if deviceLanguage.hasPrefix("en") {
+            text = "beach"
+        } else {
+            text = "我"
+        }
+        
+        tablesQuery.textFields.elementBoundByIndex(0).typeText(text)
+        snapshot("4search")
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            app.navigationBars.elementBoundByIndex(1).buttons.elementBoundByIndex(2).tap()
+        } else {
+            app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(2).tap()
+        }
+        if deviceLanguage.hasSuffix("Hans") {
+            snapshot("5result")
+        } else {
+            if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+                app.navigationBars.elementBoundByIndex(1).buttons.elementBoundByIndex(2).tap()
+            } else {
+                app.navigationBars.elementBoundByIndex(0).buttons.elementBoundByIndex(2).tap()
+            }
+            
+            snapshot("5result")
+        }
     }
     
 }
