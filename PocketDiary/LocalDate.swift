05a241c964e43a405b78300344454ffb5d277d0f
Sweeper777 <sumulang@gmail.com>
2018-08-28 11:09:21 +0100

    added LocalDate.toDate()

--- PocketDiary/LocalDate.swift
+++ PocketDiary/LocalDate.swift
@@ -5,4 +5,12 @@ class LocalDate: Object {
     @objc dynamic var year = 0
     @objc dynamic var month = 0
     @objc dynamic var day = 0
+    
+    func toDate() -> Date {
+        var dateComponents = DateComponents()
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+        return Calendar(identifier: .gregorian).date(from: dateComponents)!
+    }
 }
